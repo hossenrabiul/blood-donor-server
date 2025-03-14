c5eebaf6b0913ad9f79b31bb4fedf6c331c6a341
hossenrabiul <robiulhossen0081@gmail.com>
2025-03-14 16:10:12 +0600

    solved error

--- DataDonor/settings.py
+++ DataDonor/settings.py
@@ -129,7 +129,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
-        'USER': 'postgres.qgbgjfcxywdmgcampcba ',
+        'USER': 'postgres.qgbgjfcxywdmgcampcba',
         'PASSWORD': 'BEXXVPEBf3R690yO',
         'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
         'PORT': '6543',
